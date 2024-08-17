da9e309ea4eed468d71d6eb93d43dca547181b0e
FE4902 <isdn000123@gmail.com>
2024-08-17 13:00:27 +0900

    기능 구현 (2)

--- Mouse3/js/common.js
+++ Mouse3/js/common.js
@@ -1,16 +1,27 @@
-let x = 0,
-    y = 0;
+let x = 0;
+let targetX = 0;
+const speed = 0.3;
 
-const contentAll = document.querySelectorAll(".contWrap div");
+const contentAll = document.querySelectorAll(".contWrap img");
+
+const shadow = contentAll[0];
+const date = contentAll[1];
+const human = contentAll[2];
+const textImg = contentAll[3];
 
 window.addEventListener("mousemove", (event) => {
-    console.log(event);
-    x = event.pageX;
-    y = event.pageY;
-
-    contentAll.forEach((item) => {
-        console.log(item);
-
-        item.style.left = x + "px";
-    });
+    x = event.pageX - window.innerWidth / 2;
 });
+
+const loop = () => {
+    targetX += (x - targetX) * speed;
+
+    shadow.style.transform = `translateX(${targetX / 35}px)`;
+    date.style.transform = `translateX(${targetX / 10}px)`;
+    human.style.transform = `translateX(${-targetX / 20}px)`;
+    textImg.style.transform = `translateX(${-targetX / 10}px)`;
+
+    window.requestAnimationFrame(loop);
+};
+
+loop();
